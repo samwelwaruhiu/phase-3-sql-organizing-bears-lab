492394b0ff6c465375afa99ffd6df0e11c2c2895
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-28 14:36:02 +0300

    complited

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',3,'M','brown','high',2);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy', 3,'M','brown','high',1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 3,'M','brown','high',2);
